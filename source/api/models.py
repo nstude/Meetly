06c57b2c3553599fcfb6f328e416e7b5dcf53803
EdyKMedoedy <antonletov-777@ya.ru>
2025-03-21 17:22:27 +0400

    Добавил gir ignore, поменял поля классов в models.py

--- source/api/models.py
+++ source/api/models.py
@@ -10,26 +10,30 @@ class Profile(models.Model): # Профиль пользователя
         User,
         related_name='profile'
     )
-    bio = models.TextField( # Биография
-        blank=True
-    )
+    
     gender = models.CharField( # Пол (гендер) - male/female
         max_length=10
+    )
+    
+    photo = models.CharField( # Фотография
+        max_length=100
+    )
+    login = models.CharField(  # Логин пользователя
+        max_length=150,  
+        unique=True  
+    )
+    password = models.CharField(  # Пароль пользователя
+        max_length=150,    
+    )
+    
+    birth_date = models.DateField(  # Дата рождения
+        blank=True,  # Разрешаем оставлять пустым
+        null=True,   # Разрешаем значение NULL в базе данных
     )
 
     def __str__(self):
         return f'Профиль пользователя {self.user.username}'
 
-class Category(models.Model):  # Категория поста
-    name = models.CharField( # Название
-        max_length=100
-    )
-    description = models.TextField( # Описание
-        blank=True
-    )
-
-    def __str__(self):
-        return self.name
 
 class Post(models.Model):  # Пост
     author = models.ForeignKey( # Автор
@@ -37,20 +41,10 @@ class Post(models.Model):  # Пост
         related_name='posts',
         on_delete=models.CASCADE
     )
-    category = models.ForeignKey( # Категория
-        Category,
-        related_name='posts',
-        on_delete=models.SET_NULL, null=True
-    )
-    title = models.CharField( # Название
-        max_length=200
-    )
+    id_post = models.AutoField(primary_key=True) # иднетификатор поста
     text = models.TextField() # Текст
     created = models.DateTimeField( # Дата создания
         auto_now_add=True
-    )
-    updated = models.DateTimeField( # Дата обновления
-        auto_now=True
     )
     likes = models.ManyToManyField( # Кол-во лайков
         User,
@@ -64,35 +58,16 @@ class Post(models.Model):  # Пост
     def __str__(self):
         return self.title
 
-class Comment(models.Model):  # Коменты
-    post = models.ForeignKey(  # Пост, к которому относится комент
-        Post,
-        related_name='comments',
-        on_delete=models.CASCADE
-    )
-    author = models.ForeignKey(User, # Автор комента
-        related_name='comments',
-        on_delete=models.CASCADE
-    )
-    text = models.TextField() # Текст комента
-    created = models.DateTimeField( # Дата создания
-        auto_now_add=True
-    )
-
-    class Meta:
-        ordering = ['created']
-
-    def __str__(self):
-        return f'Комментарий от {self.author.username} на {self.post.title}'
 
 class Group(models.Model):  # Группа
     author = models.ForeignKey(User, # Автор
         related_name='groups',
         on_delete=models.CASCADE
     )
-    name = models.CharField( # Название группы
-        max_length=100
-    )
+    
+    id_group = models.AutoField( # иднетификатор поста
+        primary_key=True
+    ) 
     members = models.ManyToManyField( # Члены группы
         User,
         related_name='groups'
@@ -107,24 +82,62 @@ class Group(models.Model):  # Группа
     def __str__(self):
         return f'Группа {self.name}'
     
-class Community(models.Model):  # Сообщество
-    author = models.ForeignKey(User, # Автор
-        related_name='communitys',
+class Message(models.Model):  # Сообщение
+    message = models.ForeignKey(  
+        Post,
+        related_name='likes',
         on_delete=models.CASCADE
     )
-    name = models.CharField( # Название сообщества
-        max_length=100
+    text = models.TextField() # Текст
+    author = models.ForeignKey(User, # Автор 
+        related_name='likes',
+        on_delete=models.CASCADE
     )
-    members = models.ManyToManyField( # Члены сообщества
-        User,
-        related_name='communitys'
+    group = models.ForeignKey(User, # Группа 
+        related_name='likes',
+        on_delete=models.CASCADE
     )
-    title = models.TextField() # Описание
-    posts = models.ManyToManyField( # Посты
+    post = models.ForeignKey(User, # Пост 
+        related_name='likes',
+        on_delete=models.CASCADE
+    )
+    
+    created = models.DateTimeField( # Дата 
+        auto_now_add=True
+    )
+
+
+    def clean(self):
+        # Проверяем, сколько полей заполнено
+        non_null_fields = sum(1 for field in [self.post, self.group] if field is not None)
+
+        # Если более одного поля не NULL, выбрасываем ошибку
+        if non_null_fields > 1:
+            raise ValidationError("Только одно из полей может быть заполнено, остальные должны быть NULL.")
+
+    def save(self, *args, **kwargs):
+        # Вызываем чистку данных перед сохранением
+        self.clean()
+        super().save(*args, **kwargs)
+
+    class Meta:
+        ordering = ['created']
+
+    def __str__(self):
+        return f'Лайк от {self.author.username} на {self.post.title}'
+    
+
+class Like(models.Model):  # Лайки
+    post = models.ForeignKey(  # Пост, к которому относится лайк
         Post,
-        related_name='communitys'
+        related_name='likes',
+        on_delete=models.CASCADE
     )
-    created = models.DateTimeField( # Дата создания
+    author = models.ForeignKey(User, # Автор 
+        related_name='likes',
+        on_delete=models.CASCADE
+    )
+    created = models.DateTimeField( # Дата 
         auto_now_add=True
     )
 
@@ -132,4 +145,4 @@ class Community(models.Model):  # Сообщество
         ordering = ['created']
 
     def __str__(self):
-        return f'Сообщество {self.name}'
+        return f'Лайк от {self.author.username} на {self.post.title}'
