57828ffc031f974863e5d31f75a4cf6f8ef66272
EdyKMedoedy <antonletov-777@ya.ru>
2025-05-17 20:00:08 +0400

    Изменение аутентификации и авторизации через jwt

--- source/api/middleware.py
+++ source/api/middleware.py
@@ -5,7 +5,7 @@ from django.urls import reverse, resolve
 from django.utils.deprecation import MiddlewareMixin
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login  
-
+from django.contrib.auth.models import AnonymousUser
 
 ALLOWED_PATHS = [
     reverse('login'),  
@@ -22,29 +22,33 @@ ALLOWED_PATHS = [
 class JWTAuthMiddleware(MiddlewareMixin):
     def process_request(self, request):
         path = request.path
-        for allowed_path in ALLOWED_PATHS:
+        allowed_paths = getattr(settings, 'JWT_ALLOWED_PATHS', [])
+
+        for allowed_path in allowed_paths:
             if path.startswith(allowed_path):
-                return None  
+                return None
+
         if request.user.is_authenticated:
             return None
 
         auth_header = request.headers.get('Authorization')
+        token = None
         if auth_header and auth_header.startswith('Bearer '):
             token = auth_header.split(' ')[1]
-        else:
-            token = None
 
         if token:
-            try:
                 payload = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
-                user = User.objects.get(id=payload['user_id'])
-                user = authenticate(request, username=user.username, password=None)
-                if user is not None:
-                  login(request, user)
-                request.user = user  
-                return None  
+                user_id = payload.get('user_id')
+                if user_id is not None:
+                    user = User.objects.get(id=user_id)
+                    request.user = user
+                    return None
+                    
 
-            except (jwt.ExpiredSignatureError, jwt.DecodeError, User.DoesNotExist):
-                pass  
-            
-        return redirect(reverse('login') + '?next=' + request.path)
+        # **Ключевое изменение: Не перенаправляем на логин, если мы уже на странице логина**
+        if path != reverse('login'):  
+            return redirect(reverse('login') + '?next=' + request.path)
+        else:
+            return None 
+
+        
--- source/config/urls.py
+++ source/config/urls.py
@@ -10,8 +10,8 @@ from rest_framework_simplejwt.views import (
 )
 
 from source.api.views import (
-    ChangePasswordView, RegisterView, LogoutView,
-    index, login_view, change_password_page, friends_page
+    ChangePasswordView, RegisterView, LogoutView, LoginView,
+    index,  change_password_page, friends_page, current_user
 )
 
 # TO DO Возможно разделить урлы
@@ -20,13 +20,14 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('source.api.urls')),
     path('register/', RegisterView.as_view(), name='register'),
-    path('login/', login_view, name='login'),
+    path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('change-password/', change_password_page, name='change-password-page'),
     path('api/change-password/', ChangePasswordView.as_view(), name='change-password-api'),
+    path('current_user/', current_user, name='current_user'),
     path('meetly/friends/', friends_page, name='friends'),
      
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
