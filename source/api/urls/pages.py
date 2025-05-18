b9b98bc78016240e4c2a073b26a7062c1b52112e
Nikita <nik.str2019@mail.ru>
2025-05-18 13:51:22 +0400

    Небольшие правки с вью и urls

--- source/api/urls/pages.py
+++ source/api/urls/pages.py
@@ -2,9 +2,11 @@ from django.conf import settings
 from django.urls import path
 from django.conf.urls.static import static
 
-from source.api.views.pages import friends_page, change_password_page
+from source.api.views.pages import index, friends_page, change_password_page
 
 urlpatterns = [
+    path('index/', index, name='index-page'),
+
     path('friends/', friends_page, name='friends-page'),
     path('change-password/', change_password_page, name='change-password-page'),
 
